347042ebf229c84d3f2d12775b5f509c09011d69
Malooski <malooski@protonmail.com>
2023-12-01 00:24:06 -0600

    Don't use bun just yet :<

--- src/components/QueryDevTools.tsx
+++ src/components/QueryDevTools.tsx
@@ -1,11 +1,10 @@
 import { lazy } from "react";
 
-export const QueryDevTools =
-    process.env.NODE_ENV === "production"
-        ? () => null // Render nothing in production
-        : lazy(() =>
-              // Lazy load in development
-              import("@tanstack/react-query-devtools").then(res => ({
-                  default: res.ReactQueryDevtools,
-              })),
-          );
+export const QueryDevTools = import.meta.env.PROD
+    ? () => null // Render nothing in production
+    : lazy(() =>
+          // Lazy load in development
+          import("@tanstack/react-query-devtools").then(res => ({
+              default: res.ReactQueryDevtools,
+          })),
+      );
--- src/components/RouterDevTools.tsx
+++ src/components/RouterDevTools.tsx
@@ -1,13 +1,12 @@
 import { lazy } from "react";
 
-export const RouterDevTools =
-    process.env.NODE_ENV === "production"
-        ? () => null // Render nothing in production
-        : lazy(() =>
-              // Lazy load in development
-              import("@tanstack/router-devtools").then(res => ({
-                  default: res.TanStackRouterDevtools,
-                  // For Embedded Mode
-                  // default: res.TanStackRouterDevtoolsPanel
-              })),
-          );
+export const RouterDevTools = import.meta.env.PROD
+    ? () => null // Render nothing in production
+    : lazy(() =>
+          // Lazy load in development
+          import("@tanstack/router-devtools").then(res => ({
+              default: res.TanStackRouterDevtools,
+              // For Embedded Mode
+              // default: res.TanStackRouterDevtoolsPanel
+          })),
+      );
